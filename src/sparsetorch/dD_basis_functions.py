fix bug in sparse combination and add Fourier and Gauss derivatives

src/sparsetorch/dD_basis_functions.py
@@ -78,20 +78,25 @@ class BF_dD(torch.nn.Module):
         torch.Tensor
             interpolation matrix
         """
-        
+
         # create deques (usage like queue)
-        eval_deque = deque(bf(x) for bf,x in zip(self.bfs_1D, x))
+        eval_deque = deque(bf(x) for bf, x in zip(self.bfs_1D, x))
         level_deque = deque(bf.levels for bf in self.bfs_1D)
-        
+
         # combine functions, FIFO order
         # (apend to the right and pop from left)
         while len(eval_deque) > 1:
-            evals, levels = self.comb_meth(eval_deque.popleft(), eval_deque.popleft(), level_deque.popleft(), level_deque.popleft())
+            evals, levels = self.comb_meth(
+                eval_deque.popleft(),
+                eval_deque.popleft(),
+                level_deque.popleft(),
+                level_deque.popleft(),
+            )
             eval_deque.append(evals)
             level_deque.append(levels)
         return eval_deque.pop()
 
-        '''
+        """
         # combine functions, LIFO order
         # (append and pop from right side)
         while len(eval_deque) > 1:
@@ -99,12 +104,14 @@ class BF_dD(torch.nn.Module):
             eval_deque.append(evals)
             level_deque.append(levels)
         return eval_deque.pop()
-        '''
+        """
+
 
 class Tensorprod(BF_dD):
     """Implementation of dD basis function evaluations as Pytorch layer.
     Basis functions are combined as tensor product.
     """
+
     def get_bf_num(self):
         """Overrides interface method and returns number of basis functions
         after tensorprod combination of basis functions.
@@ -150,6 +157,7 @@ class Elemprod(BF_dD):
     """Implementation of dD basis function evaluations as Pytorch layer.
     Basis functions are combined as elementwise product.
     """
+
     def get_bf_num(self):
         """Overrides interface method and returns number of basis functions
         after elemprod combination of basis functions.
@@ -190,7 +198,8 @@ class Sparse(BF_dD):
     """Implementation of dD basis function evaluations as Pytorch layer.
     Basis functions are combined as sparse grid.
     """
-    def __get_levcombs(self, l_max):
+
+    def __get_levcombs_old(self, l_max):
         """Generator for all possible sparse level
         combination pairs for given maximum level.
         Pairs generated in ascending order of corresponding
@@ -209,6 +218,29 @@ class Sparse(BF_dD):
         for max in range(l_max + 1):
             for lev_b in range(max + 1):
                 lev_a = max - lev_b
+                yield (lev_a, lev_b)
+
+    def __get_levcombs(self, l_max_a, l_max_b):
+        """Generator for all possible sparse level
+        combination pairs for given maximum level.
+        Pairs generated in ascending order of corresponding
+        level (sum of returned level pair).
+
+        Parameters
+        ----------
+        l_max_a : int
+            maximum level of first base
+        l_max_b : int
+            maximum level of second base
+
+        Yields
+        -------
+        tuple
+            levels, two entries
+        """
+        l_max = max(l_max_a, l_max_b)
+        for lev_b in range(l_max_b + 1):
+            for lev_a in range(min(l_max_a, l_max - lev_b) + 1):
                 yield (lev_a, lev_b)
 
     def get_bf_num(self):
@@ -233,12 +265,14 @@ class Sparse(BF_dD):
             levels_new = [0] * len(levels_dD)
 
             # levelwise traversation
-            for lev_1D, lev_dD in self.__get_levcombs(len(levels_dD) - 1):
-                try:
-                    levels_new[lev_1D + lev_dD] += levels_dD[lev_dD] * bf.levels[lev_1D]
-                except IndexError:
-                    # handle case that basis functions do not have the same number of levels
-                    continue
+            for lev_1D, lev_dD in self.__get_levcombs(
+                len(bf.levels) - 1, len(levels_dD) - 1
+            ):
+                # try:
+                levels_new[lev_1D + lev_dD] += levels_dD[lev_dD] * bf.levels[lev_1D]
+                # except IndexError:
+                #    # handle case that basis functions do not have the same number of levels
+                #    continue
             levels_dD = levels_new
         return sum(levels_dD)
 
@@ -265,8 +299,10 @@ class Sparse(BF_dD):
             number of basis functions at each level in new interpolation matrix
         """
 
-        # compute highest level for computation
-        l_max = max(len(levels_a), len(levels_b)) - 1
+        # compute highest levels for computation
+        l_max_a = len(levels_a) - 1
+        l_max_b = len(levels_b) - 1
+        l_max = max(l_max_a, l_max_b)
 
         # new list with number of evaluations at each level
         levels_new = [0] * (l_max + 1)
@@ -276,7 +312,7 @@ class Sparse(BF_dD):
 
         # compute number of evaluations
         num_evals_new = 0
-        for i, j in self.__get_levcombs(l_max):
+        for i, j in self.__get_levcombs(l_max_a, l_max_b):
             num_evals_new += levels_a[i] * levels_b[j]
 
         # initialize new evaluation tensor
@@ -284,16 +320,17 @@ class Sparse(BF_dD):
 
         idx = 0  # writing index
         # levelwise traversation w.r.t. new evaluation tensor
-        for lev_a, lev_b in self.__get_levcombs(l_max):
-            try:
-                # compute index ranges for respective levels
-                start_a = sum(levels_a[:lev_a])
-                end_a = start_a + levels_a[lev_a]
-                start_b = sum(levels_b[:lev_b])
-                end_b = start_b + levels_b[lev_b]
-            except IndexError:
-                # handle case that one basis provides more levels than the other
-                continue
+        # for lev_a, lev_b in self.__get_levcombs(l_max):
+        for lev_a, lev_b in self.__get_levcombs(l_max_a, l_max_b):
+            # try:
+            # compute index ranges for respective levels
+            start_a = sum(levels_a[:lev_a])
+            end_a = start_a + levels_a[lev_a]
+            start_b = sum(levels_b[:lev_b])
+            end_b = start_b + levels_b[lev_b]
+            # except IndexError:
+            #    # handle case that one basis provides more levels than the other
+            #    continue
 
             # update level list
             levels_new[lev_a + lev_b] += levels_a[lev_a] * levels_b[lev_b]

src/sparsetorch/oneD_basis_functions.py
@@ -109,8 +109,8 @@ class BF_1D_mesh(BF_1D):
         # index shift caused by boundaries
         shift = int(boundary) * 2
 
-        mu = torch.zeros(2**(level + 1) - 1 + shift)
-        h = torch.zeros(2**(level + 1) - 1 + shift)
+        mu = torch.zeros(2 ** (level + 1) - 1 + shift)
+        h = torch.zeros(2 ** (level + 1) - 1 + shift)
         levels = [0] * (level + 1)
 
         # boundary basis functions at level 0
@@ -125,11 +125,11 @@ class BF_1D_mesh(BF_1D):
             levels[0] += 2
 
         for l in range(level + 1):
-            levels[l] += 2**l
-            h_ = 2**-l
-            for i in range(2**l):
-                mu[2**l + i - 1 + shift] = (i + 0.5) * h_
-                h[2**l + i - 1 + shift] = h_ / 2
+            levels[l] += 2 ** l
+            h_ = 2 ** -l
+            for i in range(2 ** l):
+                mu[2 ** l + i - 1 + shift] = (i + 0.5) * h_
+                h[2 ** l + i - 1 + shift] = h_ / 2
 
         # scale entries
         mu = a + mu * (b - a)
@@ -176,6 +176,49 @@ class Gauss(BF_1D_mesh):
         """
         x = torch.unsqueeze(x, -1)
         return torch.exp(-torch.square((x - self.mu) / self.h))
+
+
+class Gauss_dx(BF_1D_mesh):
+    """Implementation of 1-D Gauss basis first order derivative."""
+
+    def forward(self, x):
+        """Overrides interface method and returns tensor
+        with first order derivatives of gaussian basis function evaluations
+        \\(e^{-((x-\\mu)/h)^2}\\).
+
+        Returns
+        -------
+        torch.Tensor
+            evaluations of all basis functions in `x`
+        """
+        x = torch.unsqueeze(x, -1)
+        return (
+            -2
+            * ((x - self.mu) / self.h ** 2)
+            * torch.exp(-torch.square((x - self.mu) / self.h))
+        )
+
+
+class Gauss_dxx(BF_1D_mesh):
+    """Implementation of 1-D Gauss basis second order derivative."""
+
+    def forward(self, x):
+        """Overrides interface method and returns tensor
+        with second order derivatives of gaussian basis function evaluations
+        \\(e^{-((x-\\mu)/h)^2}\\).
+
+        Returns
+        -------
+        torch.Tensor
+            evaluations of all basis functions in `x`
+        """
+        x = torch.unsqueeze(x, -1)
+        return (
+            -2
+            * (-2 * (x - self.mu) ** 2 + self.h ** 2)
+            / self.h ** 4
+            * torch.exp(-torch.square((x - self.mu) / self.h))
+        )
 
 
 class Hat(BF_1D_mesh):
@@ -296,6 +339,85 @@ class Fourier(BF_1D_orthofun):
         return eval.T
 
 
+class Fourier_dx(Fourier):
+    """Implementation of first order derivative of Fourier series as 1-D basis."""
+
+    def __init__(self, n_max, a=0.0, b=1.0):
+        """
+        Parameters
+        ----------
+        n_max : int
+            maximum level in series
+        a : float, optional
+            left boundary of domain, by default 0.0
+        b : float, optional
+            right boundary of domain, by default 1.0
+        """
+        super().__init__(n_max, a, b)
+
+    def forward(self, x):
+        """Overrides interface method and returns tensor
+        with evaluations of first order derivatives of Fourier series elements.
+
+        Returns
+        -------
+        torch.Tensor
+            evaluations of all basis functions in `x`
+        """
+
+        x_scaled = self.scale(x)
+        eval = torch.empty(self.bf_num, len(x))
+        eval[0] = 0.0
+
+        for n in range(1, self.bf_num // 2 + 1):
+            eval[2 * n - 1] = -n * self.bf_w / self.data_w * torch.sin(n * x_scaled)
+            eval[2 * n] = n * self.bf_w / self.data_w * torch.cos(n * x_scaled)
+        return eval.T
+
+
+class Fourier_dxx(Fourier):
+    """Implementation of second order derivative of Fourier series as 1-D basis."""
+
+    def __init__(self, n_max, a=0.0, b=1.0):
+        """
+        Parameters
+        ----------
+        n_max : int
+            maximum level in series
+        a : float, optional
+            left boundary of domain, by default 0.0
+        b : float, optional
+            right boundary of domain, by default 1.0
+        """
+        super().__init__(n_max, a, b)
+
+    def forward(self, x):
+        """Overrides interface method and returns tensor
+        with evaluations of second order derivatives of Fourier series elements.
+
+        Returns
+        -------
+        torch.Tensor
+            evaluations of all basis functions in `x`
+        """
+
+        x_scaled = self.scale(x)
+        eval = torch.empty(self.bf_num, len(x))
+        eval[0] = 0.0
+
+        for n in range(1, self.bf_num // 2 + 1):
+            # apply product rule
+            duv = -((n * self.bf_w / self.data_w) ** 2) * torch.cos(n * x_scaled)
+            udv = -n * self.bf_w / self.data_w * torch.sin(n * x_scaled)
+            eval[2 * n - 1] = duv #+ udv  # u'v+uv'
+
+            # apply product rule
+            duv = -((n * self.bf_w / self.data_w) ** 2) * torch.sin(n * x_scaled)
+            udv = n * self.bf_w / self.data_w * torch.cos(n * x_scaled)
+            eval[2 * n] = duv #+ udv  # u'v+uv'
+        return eval.T
+
+
 class Chebyshev(BF_1D_orthofun):
     """Implementation of Chebyshev orthogonal polynomials as 1-D basis functions."""
 
@@ -331,7 +453,7 @@ class Chebyshev(BF_1D_orthofun):
             for n in range(2, self.bf_num):
                 # recursive definition not allowed by autograd
                 eval[n] = 2 * x * eval[n - 1].clone() - eval[n - 2].clone()
-                #eval[n] = torch.cos(n * torch.acos(x))
+                # eval[n] = torch.cos(n * torch.acos(x))
         return eval.T
 
 